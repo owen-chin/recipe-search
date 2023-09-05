d0a231a2ef15772d36804d8974329e95538c1233
Owen Chin <owenchin@nbp-49-245.nbp.ruw.rutgers.edu>
2023-09-04 20:09:56 -0400

    deployed using firebase

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCe3BXDQ_bFJ7-NudDKdDCaflKiTbB05tY",
+  authDomain: "recipe-search-52d36.firebaseapp.com",
+  projectId: "recipe-search-52d36",
+  storageBucket: "recipe-search-52d36.appspot.com",
+  messagingSenderId: "235388024544",
+  appId: "1:235388024544:web:1bcebf754da158badd4285",
+  measurementId: "G-Q38HYTPZJP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
